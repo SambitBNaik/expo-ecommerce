bc1c97b2b06ab8085c4fc755945db3ae05994567
Sambit Basant Naik <sambitbasantnaik@gmail.com>
2025-12-27 15:42:33 +0530

    Add improve edge cases at db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,14 @@ import mongoose from "mongoose";
 import { ENV } from "./env.js";
 
 export const connectDB = async() =>{
+    if(!ENV.DB_URL){
+        throw new Error("DB_URL environment variable is not defined");
+    }
     try {
         const conn = await mongoose.connect(ENV.DB_URL);
         console.log(`✅ Connected to MONGODB:${conn.connection.host}`);
     } catch (error) {
-        console.error("💥 MONGODB connection error");
+        console.error("💥 MONGODB connection error",error.message);
         process.exit(1);
     }
 }
