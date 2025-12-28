44bff98c6cbe2f718797af38f41bb638749328e7
Sambit Basant Naik <sambitbasantnaik@gmail.com>
2025-12-28 08:45:16 +0530

    Auth setup implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,14 @@ import path from 'path';
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import {clerkMiddleware} from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest,functions} from "./config/inngest.js";
 
 const app = express();
+app.use(express.json());
 app.use(clerkMiddleware());
+
+app.use("/api/inngest",serve({ client: inngest, functions}));
 
 const __dirname = path.resolve();
 app.get("/api/health",(req, res)=>{
