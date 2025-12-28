b0f8c18a3f9a56ca5a62b0e09dbe39ed86bae976
Sambit Basant Naik <sambitbasantnaik@gmail.com>
2025-12-28 17:17:42 +0530

    Implemented admin route , models and controller

---
+++ backend/src/models/review.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import { product } from "./product.model";
+
+const reviewSchema = new mongoose.Schema(
+    {
+        productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref : "Product",
+            required: true,
+        },
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref : "User",
+            required: true,
+        },
+        orderId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Order",
+            required: true,
+        },
+        ratings: {
+            type: Number,
+            required: true,
+            min: 1,
+            max: 5,
+        },
+    },
+    { timestamps: true}
+);
+
+export const Review = mongoose.model("Review", reviewSchema);
---
+++ backend/src/routes/admin.route.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { adminOnly, protectRoute } from "../middleware/auth.middleware.js";
+import { upload } from "../middleware/multer.middleware.js";
+import { createProduct, deleteProduct, getAllCustomers, getAllOrders, getAllProducts, getDashboardStats, updateOrderStatus, updateProduct } from "../controller/admin.controller.js";
+
+
+const router = Router();
+
+// optimization - Don't repeat your code
+router.use(protectRoute, adminOnly);
+
+router.post("/products", upload.array("images",3), createProduct);
+router.get("/products",getAllProducts);
+router.put("/products/:id",upload.array("images",3),updateProduct);
+router.delete("/products/:id",deleteProduct);
+
+router.get("/orders",getAllOrders);
+router.patch("/orders/:orderId/status",updateOrderStatus);
+
+router.get("/customers",getAllCustomers);
+
+router.get("/stats",getDashboardStats);
+
+export default router;
