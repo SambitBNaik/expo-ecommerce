908e718ab42ffcecdc8a719a9e83e3a0c8e9fc25
Sambit Basant Naik <sambitbasantnaik@gmail.com>
2025-12-30 21:45:34 +0530

    Dashboard page is added.

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,8 +1,31 @@
+import { UserButton } from '@clerk/clerk-react'
+import { ClipboardList, HomeIcon, PanelLeftIcon, ShoppingBagIcon, User2Icon } from 'lucide-react'
 import React from 'react'
+import { useLocation } from 'react-router'
 
+export const NAVIGATION=[
+    {name: "Dashboard", path:"/dashboard", icon: <HomeIcon className='size-5'/>},
+    {name: "Products", path:"/products", icon: <ShoppingBagIcon className='size-5'/>},
+    {name: "Orders", path:"/orders", icon: <ClipboardList className='size-5'/>},
+    {name: "Customers", path:"/customers", icon: <User2Icon className='size-5'/>}
+]
 const Navbar = () => {
+    const location = useLocation();
   return (
-    <div>Navbar</div>
+    <div className='navbar w-full bg-base-300'>
+        <label htmlFor='my-drawer' className='btn btn-square btn-ghost' aria-label='open sidebar'>
+            <PanelLeftIcon className='size-5'/>
+        </label>
+        <div className='flex-1 px-4'>
+            <h1 className='text-xl font-bold'>
+                {NAVIGATION.find((item)=>item.path === location.pathname)?.name || "Dashboard"}
+            </h1>
+        </div>
+
+        <div className='mr-5'>
+            <UserButton/>
+        </div>
+    </div>
   )
 }
 
